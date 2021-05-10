ea24bb17c4b3ffc797c88d88e7cf3c9a77af4f86
FUJITA Tomonori <fujita.tomonori@gmail.com>
2021-05-10 14:53:56 +0000

    fixes for binary build with ekidd/rust-musl-builder container
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@gmail.com>

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![recursion_limit = "1024"]
 mod api {
     tonic::include_proto!("gobgpapi");
 }
